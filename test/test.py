a993656e3c2f40621d3a62c152a59f89982e1b5b
1asset <82859085+1asset@users.noreply.github.com>
2021-11-19 10:28:11 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,10 @@
+from paragraphs import Paragraphs
 
+scrapper = Paragraphs()
+
+num = 1
+for i in scrapper.get_paragraphs('bitcoin'):
+    print(str(num) + ")     Title : " + i['title'] + ".\n    Link: "+ i['link'])
+    print("Briefly: " + i['body'])
+    print("############################################################################################################")
+    num+=1
